6a75950063da1d8e1cec3e776cfd0211410b4911
Marvin Hagemeister <marvin@marvinhagemeister.de>
2019-03-19 12:31:19 +0100

    Fix svg test again

--- compat/test/browser/svg.test.js
+++ compat/test/browser/svg.test.js
@@ -27,7 +27,7 @@ describe('svg', () => {
 	it('should render SVG to DOM #1', () => {
 		const Demo = () => (
 			<svg viewBox="0 0 360 360">
-				<path stroke="white" fill="black" d="M347.1 357.9L183.3 256.5 L 13 357.9V1.7h334.1v356.2zM58.5 47.2v231.4l124.8-74.1 118.3 72.8V47.2H58.5z" />
+				<path stroke="white" fill="black" d="M347.1 357.9L183.3 256.5 L 13 357.9V1.7h334.1v356.2zM58.5 47.2v231.4l124.8-74.1 l 118.3 72.8V47.2H58.5z" />
 			</svg>
 		);
 		React.render(<Demo />, scratch);
